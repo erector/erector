e455fb414422b75638059321a38fcfb2c23d77de
nkallen <nkallen@4b8798b9-51f2-4387-91b1-b04f834b421f>
2008-03-27 23:46:06 +0000

    rspec fix
    git-svn-id: svn+ssh://rubyforge.org/var/svn/erector/trunk@35 4b8798b9-51f2-4387-91b1-b04f834b421f

--- lib/erector/extensions/action_controller.rb
+++ lib/erector/extensions/action_controller.rb
@@ -9,11 +9,11 @@ class ActionController::Base
     @rendered_widget.to_s
   end
   
-  def render_with_erector_widget(options = nil, &block)
-    if options.is_a?(Hash) && widget = options.delete(:widget)
-      render_widget widget, options, &block
+  def render_with_erector_widget(*options, &block)
+    if options.first.is_a?(Hash) && widget = options.first.delete(:widget)
+      render_widget widget, *options, &block
     else
-      render_without_erector_widget options, &block
+      render_without_erector_widget *options, &block
     end
   end
   alias_method_chain :render, :erector_widget
