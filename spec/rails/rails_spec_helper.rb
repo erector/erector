2e613c90f35b82eece8f31419ef5c82fd2f01d3f
btakita <btakita@4b8798b9-51f2-4387-91b1-b04f834b421f>
2008-06-06 01:38:20 +0000

    Moving rails specific requires into rails_spec_helper.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/erector/trunk@152 4b8798b9-51f2-4387-91b1-b04f834b421f

--- spec/rails/rails_spec_helper.rb
+++ spec/rails/rails_spec_helper.rb
@@ -1,3 +1,6 @@
 dir = File.dirname(__FILE__)
 RAILS_ROOT = File.expand_path("#{dir}/rails_root")
 require File.expand_path("#{dir}/../spec_helper")
+require "active_record"
+#require "erector/rails"
+require "action_controller/test_process"
