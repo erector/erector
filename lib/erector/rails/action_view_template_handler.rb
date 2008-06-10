31bb34959096f0e07c8920e180dbd6ac0df0ecde
btakita <btakita@4b8798b9-51f2-4387-91b1-b04f834b421f>
2008-06-10 09:43:16 +0000

    Specs pass for Rails 2.1.0
    git-svn-id: svn+ssh://rubyforge.org/var/svn/erector/trunk@176 4b8798b9-51f2-4387-91b1-b04f834b421f

--- lib/erector/rails/action_view_template_handler.rb
+++ lib/erector/rails/action_view_template_handler.rb
@@ -10,39 +10,83 @@ module ActionView #:nodoc:
         @view = view
       end
 
-      def compile(template)
-        relative_path_parts = view.first_render.split('/')
-        require_dependency view.template_file_path
-
-        dot_rb = /\.rb$/
-        widget_class_parts = relative_path_parts.inject(['Views']) do |class_parts, node|
-          class_parts << node.gsub(dot_rb, '').camelize
-          class_parts
-        end
-        widget_class_name = widget_class_parts.join("::")
-        render_method = view.is_partial_template? ? 'render_partial' : 'render'
-
-        erb_template = "<% #{widget_class_name}.new(self, controller.assigns, StringIO.new(_erbout)).#{render_method} %>"
-        ::ERB.new(erb_template, nil, @view.erb_trim_mode).src
+      def compilable?
+        true
       end
 
-      def render(template, local_assigns)
-        relative_path_parts = view.first_render.split('/')
-        require_dependency view.template_file_path
-
-        dot_rb = /\.rb$/
-        widget_class = relative_path_parts.inject(Views) do |mod, node|
-          mod.const_get(node.gsub(dot_rb, '').camelize)
+      if ActionView.const_defined?(:Template)
+        ActionView::Template.instance_eval do
+          register_template_handler :rb, ActionView::TemplateHandlers::Erector
         end
-        render_method = view.is_partial_template? ? 'render_partial' : 'render'
-        widget_class.new(view, view.assigns).to_s(render_method)
+
+        def compile_template(template)
+          relative_path_parts = view.first_render.split('/')
+          require_dependency view.finder.pick_template(
+            template.path,
+            view.finder.pick_template_extension(template.path)
+          )
+
+          dot_rb = /\.rb$/
+          widget_class_parts = relative_path_parts.inject(['Views']) do |class_parts, node|
+            class_parts << node.gsub(dot_rb, '').camelize
+            class_parts
+          end
+          widget_class_name = widget_class_parts.join("::")
+          render_method = template.is_a?(ActionView::PartialTemplate) ? 'render_partial' : 'render'
+
+          erb_template = "<% #{widget_class_name}.new(self, controller.assigns, StringIO.new(_erbout)).#{render_method} %>"
+          ::ERB.new(erb_template, nil, @view.erb_trim_mode).src
+        end
+
+        def render(template)
+          relative_path_parts = view.first_render.split('/')
+          require_dependency view.finder.pick_template(
+            template.path,
+            view.finder.pick_template_extension(template.path)
+          )
+
+          dot_rb = /\.rb$/
+          widget_class = relative_path_parts.inject(Views) do |mod, node|
+            mod.const_get(node.gsub(dot_rb, '').camelize)
+          end
+          render_method = template.is_a?(ActionView::PartialTemplate) ? 'render_partial' : 'render'
+          widget_class.new(view, view.assigns).to_s(render_method)
+        end
+      else
+        ActionView::Base.instance_eval do
+          if respond_to?(:register_template_handler)
+            register_template_handler :rb, ActionView::TemplateHandlers::Erector
+          end
+        end
+
+        def compile(template)
+          relative_path_parts = view.first_render.split('/')
+          require_dependency view.template_file_path
+
+          dot_rb = /\.rb$/
+          widget_class_parts = relative_path_parts.inject(['Views']) do |class_parts, node|
+            class_parts << node.gsub(dot_rb, '').camelize
+            class_parts
+          end
+          widget_class_name = widget_class_parts.join("::")
+          render_method = view.is_partial_template? ? 'render_partial' : 'render'
+
+          erb_template = "<% #{widget_class_name}.new(self, controller.assigns, StringIO.new(_erbout)).#{render_method} %>"
+          ::ERB.new(erb_template, nil, @view.erb_trim_mode).src
+        end
+
+        def render(template, local_assigns)
+          relative_path_parts = view.first_render.split('/')
+          require_dependency view.template_file_path
+
+          dot_rb = /\.rb$/
+          widget_class = relative_path_parts.inject(Views) do |mod, node|
+            mod.const_get(node.gsub(dot_rb, '').camelize)
+          end
+          render_method = view.is_partial_template? ? 'render_partial' : 'render'
+          widget_class.new(view, view.assigns).to_s(render_method)
+        end
       end
     end
-  end
-end
-
-ActionView::Base.instance_eval do
-  if respond_to?(:register_template_handler)
-    register_template_handler :rb, ActionView::TemplateHandlers::Erector
   end
 end
--- lib/erector/rails/supported_rails_versions.rb
+++ lib/erector/rails/supported_rails_versions.rb
@@ -4,7 +4,7 @@ module Erector
 #      "1.2.5" => {'version' => '1.2.5', 'git_tag' => 'v1.2.5'},
       "1.99.0" => {'version' => '1.99.0', 'git_tag' => 'v2.0.0_RC1'},
       "2.0.2" => {'version' => '2.0.2', 'git_tag' => 'v2.0.2'},
-#      "2.1.0" => {'version' => '2.1.0', 'git_tag' => 'v2.1.0'},
+      "2.1.0" => {'version' => '2.1.0', 'git_tag' => 'v2.1.0'},
 #      "edge" => {'version' => 'edge', 'git_tag' => 'master'},
     }
   end
