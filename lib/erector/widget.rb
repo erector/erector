f9caeec0c747d7c3f08b10fdc4becae39d5d9003
Corey Innis <corey@coolerator.net>
2008-11-20 15:24:54 -0800

    added col, colgroup, and tfoot to Widget

--- lib/erector/widget.rb
+++ lib/erector/widget.rb
@@ -32,14 +32,14 @@ module Erector
       # tags which can contain other stuff
       def full_tags
         [
-          'a', 'acronym', 'address', 'b', 'bdo', 'big', 'blockquote', 'body',
-          'button', 'caption', 'center', 'cite', 'code',
+          'a', 'abbr', 'acronym', 'address', 'b', 'bdo', 'big', 'blockquote', 'body',
+          'button', 'caption', 'center', 'cite', 'code', 'col', 'colgroup',
           'dd', 'del', 'div', 'dl', 'dt', 'em',
           'fieldset', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'html', 'i',
           'iframe', 'ins', 'kbd', 'label', 'legend', 'li', 'map',
           'noframes', 'noscript', 'ol', 'optgroup', 'option', 'p', 'param', 'pre',
           'samp', 'script', 'select', 'small', 'span', 'strong', 'style', 'sub', 'sup',
-          'table', 'tbody', 'td', 'textarea', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'var'
+          'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'var'
         ]
       end
 
