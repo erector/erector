c06f2b7d760415b6946a36f09b2ad2eb1acc79b6
Jim Kingdon <jkingdon@localhost.localdomain>
2009-01-08 12:10:38 -0500

    Add rdoc for Erector::Widgets::Table.

--- lib/erector/widgets/table.rb
+++ lib/erector/widgets/table.rb
@@ -1,23 +1,40 @@
 module Erector
   module Widgets #:nodoc:
+
+    # The Table widget provides the ability to render a table from a 
+    # list of objects (one for each row).
+    #
+    # Because the default for the column titles is to call the rails
+    # humanize and titleize methods, this widget is rails-specific.
     class Table < Erector::Widget
       ColumnDefinition = Struct.new(:id, :name, :cell_proc)
       class << self
+        # Define a column, optionally specifying the name (the heading
+        # that the user sees) and a block which renders the cell given
+        # a row object.  If the block is not specified, the cell contains
+        # the result of calling a method whose name is id.
+        # 
+        # The name can be a string or a proc.
         def column(id, name=id.to_s.humanize.titleize, &cell_proc)
           cell_proc ||= proc {|object| text object.__send__(id)}
           column_definitions << ColumnDefinition.new(id, name, cell_proc)
         end
 
-        def column_definitions
+        def column_definitions #:nodoc:
           @column_definitions ||= []
         end
 
+        # A list of HTML classes to apply to the rows in turn.  After the
+        # list is exhausted, start again at the start.  The most
+        # common use for this is to specify one class for odd rows
+        # and a different class for even rows.
         def row_classes(*row_classes)
           @row_class_list = row_classes
         end
         attr_reader :row_class_list
       end
 
+      # The standard erector render method.
       def render
         table do
           thead do
@@ -42,7 +59,7 @@ module Erector
       end
 
       protected
-      def row(object, index)
+      def row(object, index) #:nodoc:
         tr(:class => row_css_class(object, index)) do
           column_definitions.each do |column_def|
             td do
@@ -52,16 +69,17 @@ module Erector
         end
       end
 
-      # Im overridable
+      # You can override this method to provide a class for a row
+      # (as an alternative to calling row_classes).
       def row_css_class(object, index)
         cycle(index)
       end
 
-      def column_definitions
+      def column_definitions #:nodoc:
         self.class.column_definitions
       end
 
-      def cycle(index)
+      def cycle(index) #:nodoc:
         list = self.class.row_class_list
         list ? list[index % list.length] : ''
       end
