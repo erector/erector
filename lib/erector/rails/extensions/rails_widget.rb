94f709fff14c61acca60b53e969c150fe2a310cf
Brian Takita <btakita@btakita-laptop.(none)>
2009-04-09 01:16:46 -0700

    Works with Rails 2.2.2

--- lib/erector/rails/extensions/rails_widget.rb
+++ lib/erector/rails/extensions/rails_widget.rb
@@ -1,12 +1,38 @@
 module Erector
   class RailsWidget < Widget
+    def output
+      process_output_buffer || @output
+    end
+
+    def capture_with_helpers(&block)
+      helpers ? helpers.capture(&block) : capture_without_helpers(&block)
+    end
+
+    alias_method_chain :capture, :helpers
+
+    # This is here to force #helpers.capture to return the output
+    def __in_erb_template; end
+
+    private
+
+    def process_output_buffer
+      if helpers.respond_to?(:output_buffer)
+        buffer = helpers.output_buffer
+        buffer.is_a?(String) ? buffer : handle_rjs_buffer
+      else
+        nil
+      end
+    end
+
+    def handle_rjs_buffer
+      returning buffer = helpers.output_buffer.dup.to_s do
+        helpers.output_buffer.clear
+        helpers.with_output_buffer(buffer) do
+          buffer << helpers.output_buffer.to_s
+        end
+      end
+    end
   end
 end
 
-dir = File.dirname(__FILE__)
-require "#{dir}/rails_widget/helpers"
-if ActionView::Base.instance_methods.include?("output_buffer")
-  require "#{dir}/rails_widget/2.2.0/rails_widget"
-else
-  require "#{dir}/rails_widget/1.2.5/rails_widget"
-end
+require "#{File.dirname(__FILE__)}/rails_widget/helpers"
