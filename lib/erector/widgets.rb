2cbcf08df6a807b84fb851f3278791dfb69dd7d8
Alex Chaffee <alex@stinky.com>
2009-10-17 12:37:58 -0700

    add EnvironmentBadge widget to requirements

--- lib/erector/widgets.rb
+++ lib/erector/widgets.rb
@@ -1,3 +1,4 @@
 require "#{File.dirname(__FILE__)}/widgets/table"
 require "#{File.dirname(__FILE__)}/widgets/field_table"
 require "#{File.dirname(__FILE__)}/widgets/page"
+require "#{File.dirname(__FILE__)}/widgets/environment_badge"
