be0db78fad0cdec1393a77b1dab3601df01b6593
John Firebaugh <john_firebaugh@bigfix.com>
2009-10-25 17:37:01 -0700

    Merge template_handler_spec into render_widget_spec

--- spec/rails_root/app/views/template_handler_specs/action_with_instance_variables_being_passed_into_render_call.html.rb
+++
@@ -1,5 +0,0 @@
-class Views::TemplateHandlerSpecs::ActionWithInstanceVariablesBeingPassedIntoRenderCall < Erector::RailsWidget
-  def content
-    div "Value of @foo is #{@foo}", :class => 'page'
-  end
-end
--- spec/rails_root/spec/extensions/render_widget_spec.rb
+++ spec/rails_root/spec/extensions/render_widget_spec.rb
@@ -16,6 +16,15 @@ describe ActionController::Base do
       render :widget => TestWidget
     end
 
+    def render_colon_widget_with_explicit_assigns
+      render :widget => TestWidget, :foobar => "foobar"
+    end
+
+    def render_colon_template
+      @foo = "foo"
+      render :template => "template_handler_specs/test_page.html.rb"
+    end
+
     def render_rjs_with_widget
       render :update do |page|
         page.insert_html :top, 'foobar', TestFormWidget.new(:parent => self).to_s
@@ -63,6 +72,20 @@ describe ActionController::Base do
       @request.action = "render_colon_widget_with_implict_assigns"
       @controller.process(@request, @response)
       @response.body.should == "foobar"
+    end
+
+    it "should render a widget with explicit assigns" do
+      @request.action = "render_colon_widget_with_explicit_assigns"
+      @controller.process(@request, @response)
+      @response.body.should == "foobar"
+    end
+  end
+
+  describe "#render :template" do
+    it "assigns instance variables, renders partials, and properly handles controllers with pluralized names" do
+      @request.action = "render_colon_template"
+      @controller.process(@request, @response)
+      @response.body.strip.gsub("  ", "").gsub("\n", "").should == '<div class="page"><div class="partial">foo</div></div>'
     end
   end
 
