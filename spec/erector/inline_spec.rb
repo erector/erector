c895603a0818448d3317d31d8c78395fa188a31b
John Firebaugh <john_firebaugh@bigfix.com>
2009-10-26 17:45:55 -0700

    Cleanup and add specs
    Test one thing per example
    Test Inline#method_missing magic
    Test things that were previously asserted in comments

--- spec/erector/inline_spec.rb
+++ spec/erector/inline_spec.rb
@@ -1,51 +1,69 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
-require 'benchmark'
 
-describe "passing in a block" do
-  describe Erector::Widget do
-    it "'s block is evaluated in the calling object's context" do
-      
-      @sample_instance_variable = "yum"
-      sample_bound_variable = "yay"
-      Erector::Widget.new do
-        @sample_instance_variable.should == "yum"
-        sample_bound_variable.should == "yay"
-        lambda {text "you can't call Erector methods from in here"}.should raise_error(NoMethodError)
-        # puts "uncomment this to prove this is being executed"
-      end.to_s
-      
-    end
+describe Erector::Widget do
+  it "provides access to instance variables from the calling context" do
+    @var = "yay"
+    Erector::Widget.new { @var.should == "yay"; @var = "yum" }.to_s
+    @var.should == "yum"
   end
 
-  describe Erector::Inline do
-    it "'s block is evaluated in the widget's context" do
-      
-      @sample_instance_variable = "yum"
-      sample_bound_variable = "yay"
-      Erector.inline do
-        @sample_instance_variable.should be_nil
-        sample_bound_variable.should == "yay"
-        text "you can call Erector methods from in here"
-        # puts "uncomment this to prove this is being executed"
-      end.to_s
-      
-    end
+  it "provides access to bound variables from the calling context" do
+    var = "yay"
+    Erector::Widget.new { var.should == "yay"; var = "yum" }.to_s
+    var.should == "yum"
   end
-  
-  describe Erector::Mixin do
-    include Erector::Mixin
-    it "'s block is evaluated in the parent widget's context" do
-      
-      @sample_instance_variable = "yum"
-      sample_bound_variable = "yay"
-      erector do
-        @sample_instance_variable.should be_nil
-        sample_bound_variable.should == "yay"
-        text "you can call Erector methods from in here"
-        # puts "uncomment this to prove this is being executed"
-      end
-      
-    end
+
+  it "doesn't provide access to Erector methods" do
+    Erector::Widget.new { lambda { text "yay" }.should raise_error(NoMethodError) }.to_s
   end
-    
+end
+
+describe Erector::Inline do
+  it "returns an InlineWidget" do
+    Erector.inline.should be_a_kind_of(Erector::InlineWidget)
+  end
+
+  it "doesn't provide access to instance variables from the calling context" do
+    @var = "yay"
+    Erector.inline { text @var }.to_s.should == ""
+  end
+
+  it "provides access to bound variables from the calling context" do
+    var = "yay"
+    Erector.inline { text var }.to_s.should == "yay"
+  end
+
+  it "provides access to explicit assigns" do
+    Erector.inline(:var => "yay") { text @var }.to_s.should == "yay"
+  end
+
+  it "provides access to methods from the calling context" do
+    def helper
+      "yay"
+    end
+
+    Erector.inline { text helper }.to_s.should == "yay"
+  end
+end
+
+describe Erector::Mixin do
+  include Erector::Mixin
+
+  it "doesn't provide access to instance variables from the calling context" do
+    @var = "yay"
+    erector { text @var }.should == ""
+  end
+
+  it "provides access to bound variables from the calling context" do
+    var = "yay"
+    erector { text var }.should == "yay"
+  end
+
+  it "provides access to methods from the calling context" do
+    def helper
+      "yay"
+    end
+
+    erector { text helper }.should == "yay"
+  end
 end
