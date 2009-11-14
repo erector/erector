d018ffbd0616a307853523dc830fc1f49d491850
Alex Chaffee <alex@stinky.com>
2009-11-14 13:24:49 -0800

    the CI box seems to be running Ruby 1.8.6, so backport Dir.mktmpdir to try to get this test to pass

--- spec/erect/erect_rails_spec.rb
+++ spec/erect/erect_rails_spec.rb
@@ -1,6 +1,48 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 require "erector/rails"
 
+# backport mktmpdir so this test will work on Ruby 1.8.6
+unless Dir.respond_to?(:mktmpdir)
+  def Dir.mktmpdir(prefix_suffix=nil, tmpdir=nil)
+    case prefix_suffix
+    when nil
+      prefix = "d"
+      suffix = ""
+    when String
+      prefix = prefix_suffix
+      suffix = ""
+    when Array
+      prefix = prefix_suffix[0]
+      suffix = prefix_suffix[1]
+    else
+      raise ArgumentError, "unexpected prefix_suffix: #{prefix_suffix.inspect}"
+    end
+    tmpdir ||= Dir.tmpdir
+    t = Time.now.strftime("%Y%m%d")
+    n = nil
+    begin
+      path = "#{tmpdir}/#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"
+      path << "-#{n}" if n
+      path << suffix
+      Dir.mkdir(path, 0700)
+    rescue Errno::EEXIST
+      n ||= 0
+      n += 1
+      retry
+    end
+
+    if block_given?
+      begin
+        yield path
+      ensure
+        FileUtils.remove_entry_secure path
+      end
+    else
+      path
+    end
+  end
+end
+
 # Note: this is *not* inside the rails_root since we're not testing 
 # Erector inside a rails app. We're testing that we can use the command-line
 # converter tool on a newly generated scaffold app (like we brag about in the 
