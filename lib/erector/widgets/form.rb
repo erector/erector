ef4e4985c6e849f096c2435b025c7a925f9ea17f
Alex Chaffee <alex@stinky.com>
2009-12-01 08:59:55 -0800

    form improvements, sadly untested

--- lib/erector/widgets/form.rb
+++ lib/erector/widgets/form.rb
@@ -1,10 +1,13 @@
 # todo: make more like http://github.com/justinfrench/formtastic
 
 class Form < Erector::Widget
-  needs :action, :method => "post"
+  needs :action, :method => "post", :onsubmit => nil, :data => nil
   
   def content
-    form :method => form_method, :action => @action do
+    opts = {:method => form_method, :action => @action}
+    opts.merge!(:data => @data) if @data
+    opts.merge!(:onsubmit => @onsubmit) if @onsubmit
+    form opts do
       unless rest_method == form_method
         input :type => "hidden", :name => "_method", :value => rest_method
       end
