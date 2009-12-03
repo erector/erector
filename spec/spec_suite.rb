e352ca9d3a1ce7897bbb98744b26a8a78ce49d58
Alex Chaffee <alex@stinky.com>
2009-12-03 07:16:03 -0800

    ActionController::Base omits a few variables from its 'protected_instance_variables' list, so these were getting improperly passed to widgets and tripping up the 'needs' alert.

--- spec/spec_suite.rb
+++ spec/spec_suite.rb
@@ -6,6 +6,7 @@ class SpecSuite
       require "#{dir}/../lib/erector/rails/rails_version"
 
       rails_version = Erector::Rails::RAILS_VERSION
+      puts ""
       puts "Running rails_spec_suite for Rails version #{rails_version}"
 
       system("ruby #{dir}/rails_spec_suite.rb") || raise("Failed for version #{rails_version}")
