dcbbf007f38bc93ac785ab565d6f7494d10940e0
John Firebaugh <john_firebaugh@bigfix.com>
2010-05-28 15:48:30 -0700

    Revert ! idiom change for RailsFormBuilder; it's on the bang_idiom branch.

--- lib/erector/rails/rails_form_builder.rb
+++ lib/erector/rails/rails_form_builder.rb
@@ -8,20 +8,12 @@ module Erector
     end
 
     def method_missing(method_name, *args, &block)
-      if method_name.to_s =~ /!$/ && parent.respond_to?(method_name.to_s.gsub(/!$/, ""))
-        return_value = parent.send(method_name.to_s.gsub(/!$/, ""), *args, &block)
+      if parent.respond_to?(method_name)
+        return_value = parent.send(method_name, *args, &block)
         template.concat(return_value) if return_value.is_a?(String)
         return_value
-      elsif parent.respond_to?(method_name)
-        parent.send(method_name, *args, &block)
       else
         super
-      end
-    end
-
-    [:fields_for, :form_for].each do |disallow_bang_method_base_name|
-      define_method "#{disallow_bang_method_base_name}!" do |*args|
-        raise "#{disallow_bang_method_base_name}! is not allowed. Only #{disallow_bang_method_base_name} is allowed."
       end
     end
   end
