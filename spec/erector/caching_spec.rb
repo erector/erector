14513c3759107720854469ec742cf015b7fd0dee
Alex Chaffee <alex@stinky.com>
2010-06-16 10:13:24 -0700

    oops

--- spec/erector/caching_spec.rb
+++ spec/erector/caching_spec.rb
@@ -1,6 +1,5 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 
-if Erector::Cache.is_supported?
   describe Erector::Cache do
     before do
       @cache = Erector::Cache.new
@@ -266,4 +265,3 @@ if Erector::Cache.is_supported?
 
     end
   end
-end
