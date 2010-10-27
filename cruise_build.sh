be12685391033d8a510a9c7cc35e8f39bdc4d643
Alex Chaffee <alex@stinky.com>
2010-10-27 13:15:13 -0700

    install ruby 1.8.6 on CI box

--- cruise_build.sh
+++ cruise_build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-source $HOME/.rvm/scripts/rvm && source .rvmrc
+source $HOME/.rvm/scripts/rvm || exit 1
+
+rvm use 1.8.6 || rvm install 1.8.6 || exit 1
+source .rvmrc
+rvm list
 
 echo USER=$USER && ruby --version && which ruby && which bundle
-
-rvm list
 
 bundle check || bundle install &&
   rake cruise
