8b49893232ff71bf025fc4113cc474ca2c935a89
Alex Chaffee <alex@stinky.com>
2010-10-27 13:09:23 -0700

    show me the rubies

--- cruise_build.sh
+++ cruise_build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 source $HOME/.rvm/scripts/rvm && source .rvmrc
+
 echo USER=$USER && ruby --version && which ruby && which bundle
+
+rvm list
+
 bundle check || bundle install &&
   rake cruise
