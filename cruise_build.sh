8092ade86a031c3c316a8f3043c2f9b8313a7e54
Alex Chaffee <alex@stinky.com>
2010-10-27 14:22:36 -0700

    resume spamming the mailing list with CI failures

--- cruise_build.sh
+++ cruise_build.sh
@@ -24,9 +24,6 @@ remove_annoying_warning
 # install bundler if necessary
 gem list --local bundler | grep bundler || gem install bundler || exit 1
 
-# debugging info
-echo USER=$USER && ruby --version && which ruby && which bundle
-
 # conditionally install project gems from Gemfile
 bundle check || bundle install || exit 1
 
