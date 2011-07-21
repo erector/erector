7c0a506d3a3f3769bcf9f89b715c4067f57bd19d
Alex Chaffee <alex@stinky.com>
2011-07-21 15:45:35 -0700

    bump version to 0.9.0.pre

--- lib/erector/version.rb
+++ lib/erector/version.rb
@@ -6,7 +6,7 @@ module Erector
   if !Erector.const_defined?(:VERSION)
     dir = File.dirname(__FILE__)
     version = YAML.load_file(File.expand_path("#{dir}/../../VERSION.yml"))
-    VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}"
+    VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}#{'.'+version[:release] if version[:release]}"
   end
 end
 
