4c4cb7323b10b13db429de80b52bb72200da791b
zelig <viktor.tron@gmail.com>
2011-07-28 14:45:10 +0100

    redefine widget (inhrited AbstractWidget) to delegate to render :widget in order not to bypass controller assignments

--- spec/rails_root/app/views/layouts/erector_layout_with_nested_widget.rb
+++ spec/rails_root/app/views/layouts/erector_layout_with_nested_widget.rb
@@ -2,7 +2,7 @@ class Views::Layouts::ErectorLayoutWithNestedWidget < Erector::Widget
   needs :layout_need => 'erector'
   
   def content
-    render :widget => Views::Test::RenderDefault
+    widget Views::Test::RenderDefault
     text 'nested in '
     rawtext content_for(:layout)
     text "with #{@layout_need} layout"
