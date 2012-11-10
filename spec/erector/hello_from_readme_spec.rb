1b25106c8d7a66e91ff4acec3397186491b4e7ea
zelig <viktor.tron@gmail.com>
2012-11-10 16:28:52 +0000

    hello_from_readme test fixed to work outside bundle/rvm

--- spec/erector/hello_from_readme_spec.rb
+++ spec/erector/hello_from_readme_spec.rb
@@ -3,9 +3,9 @@ require File.expand_path("#{here}/../spec_helper")
 
 describe "Hello World example from README" do
   it "works" do
-    Dir.chdir(here) do
+    Dir.chdir('/tmp') do
       clear_bundler_env
-      html = sys "ruby hello_from_readme.rb"
+      html = `ruby #{File.join(here,'hello_from_readme.rb')} 2>&1`
       html.should == "<html><head><title>Welcome page</title></head><body><p>Hello, world</p></body></html>\n"
     end
   end
