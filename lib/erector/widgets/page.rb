2b37ac9c3e21d98d3a6430867d07002c6676d629
Christopher Lord <christopherlord@gmail.com>
2013-01-16 15:06:43 -0700

    add initial implementation

--- lib/erector/widgets/page.rb
+++ lib/erector/widgets/page.rb
@@ -100,11 +100,48 @@
 #
 class Erector::Widgets::Page < Erector::InlineWidget
 
-  # Emit the Transitional doctype.
-  # TODO: allow selection from among different standard doctypes
+  # Emit the desired doctype. Defaults to HTML5 transitional
+  def standard_doctype(format = :html5, dt = :transitional)
+     inside = case format
+     when :html5, :xhtml5
+        ""
+     when :html4
+        case dt
+        when :transitional
+          'PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"'
+        when :strict
+          'PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"'
+        when :frameset
+          'PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd"'
+        end
+     when :xhtml10
+       case dt
+       when :transitional
+         'PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"'
+       when :strict
+         'PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"'
+       when :frameset
+         'PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"'
+       when :html5
+         ''
+       end
+     when :xhtml11
+       'PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"'
+     when :xhtml20
+       'PUBLIC "-//W3C//DTD XHTML 2.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml2.dtd"'
+     when :basic
+       'PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd"'
+     when :mobile
+       'PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd"'
+     when :rdfa
+       'PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"'
+     end
+     "<!DOCTYPE html#{inside}>"
+  end
+
+  # Override to change the document's doctype
   def doctype
-    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+    standard_doctype(:xhtml10, :transitional)
   end
 
   def content
