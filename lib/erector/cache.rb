2fc60d5e52934997dc8f54b0f466e390b5a47cf9
adamjacobbecker <ad@mbecker.cc>
2013-12-31 12:57:03 -0800

    delegate cache key generation to ActiveSupport::Cache

--- lib/erector/cache.rb
+++ lib/erector/cache.rb
@@ -18,19 +18,7 @@ module Erector
     end
 
     def transform_key(args)
-      ['erector'] + args.reject { |x| x.nil? }.map { |x|
-        if x.is_a?(Hash)
-          transformed = {}
-
-          x.each do |k, v|
-            transformed[k] = v.respond_to?(:cache_key) ? v.cache_key : v
-          end
-
-          transformed
-        else
-          x
-        end
-      }
+      ActiveSupport::Cache.expand_cache_key(args.reject { |x| x.nil? }, 'erector')
     end
 
   end
