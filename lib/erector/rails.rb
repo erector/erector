7acacb16b5d43ab2e570a0706beab0f02eace8c2
Alex Chaffee <alex@stinky.com>
2014-04-14 17:46:06 -0700

    guard against Rails 4 for the moment

--- lib/erector/rails.rb
+++ lib/erector/rails.rb
@@ -1,7 +1,9 @@
 require 'rails/version'
 if Rails::VERSION::MAJOR == 2
   require 'erector/rails2'
-else
+elsif Rails::VERSION::MAJOR == 3
   require 'erector/rails3'
+else
+  exit "Rails 4 not yet supported"
+  require 'erector/rails4'
 end
-
