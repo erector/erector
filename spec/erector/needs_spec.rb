519df0ed29076f9cac720b83f073031d24485a90
adamjacobbecker <ad@mbecker.cc>
2014-04-27 17:21:21 -0700

    remove deprecated `should_not raise_error(SpecificErrorClass)` syntax

--- spec/erector/needs_spec.rb
+++ spec/erector/needs_spec.rb
@@ -11,7 +11,7 @@ describe Erector::Needs do
     class Thing2 < Erector::Widget
       needs nil
     end
-    lambda { Thing2.new }.should_not raise_error(ArgumentError)
+    lambda { Thing2.new }.should_not raise_error
     lambda { Thing2.new(:foo => 1) }.should raise_error(ArgumentError)
   end
 
@@ -19,7 +19,7 @@ describe Erector::Needs do
     class Thing2b < Erector::Widget
       needs :foo
     end
-    lambda { Thing2b.new(:foo => 1) }.should_not raise_error(ArgumentError)
+    lambda { Thing2b.new(:foo => 1) }.should_not raise_error
   end
 
   it "complains if you pass it an undeclared parameter" do
@@ -33,7 +33,7 @@ describe Erector::Needs do
     class Thing4 < Erector::Widget
       needs :foo, :bar
     end
-    lambda { Thing4.new(:foo => 1, :bar => 2) }.should_not raise_error(ArgumentError)
+    lambda { Thing4.new(:foo => 1, :bar => 2) }.should_not raise_error
   end
 
   it "complains when passing in an extra parameter after declaring many parameters" do
@@ -60,7 +60,7 @@ describe Erector::Needs do
       thing = Thing7.new(:foo => 1, :baz => 3)
       thing.instance_variable_get(:@bar).should equal(7)
       thing.instance_variable_get(:@baz).should equal(3)
-    }.should_not raise_error(ArgumentError)
+    }.should_not raise_error
   end
 
   it "allows multiple values on a line, including default values at the end of the line" do
@@ -72,7 +72,7 @@ describe Erector::Needs do
       thing.instance_variable_get(:@foo).should equal(1)
       thing.instance_variable_get(:@bar).should equal(7)
       thing.instance_variable_get(:@baz).should equal(2)
-    }.should_not raise_error(ArgumentError)
+    }.should_not raise_error
   end
 
   it "duplicates default values, so they're not accidentally shared across all instances" do
@@ -95,7 +95,7 @@ describe Erector::Needs do
     lambda {
       thing = Thing9.new
       thing.instance_variable_get(:@foo).should be_nil
-    }.should_not raise_error(ArgumentError)
+    }.should_not raise_error
   end
 
   it "doesn't attempt to dup undupable value if there's another need passed in (bug)" do
@@ -119,7 +119,7 @@ describe Erector::Needs do
       needs :engine
     end
 
-    lambda { Car.new(:engine => 'V-8', :wheels => 4) }.should_not raise_error(ArgumentError)
+    lambda { Car.new(:engine => 'V-8', :wheels => 4) }.should_not raise_error
     lambda { Car.new(:engine => 'V-8') }.should raise_error(ArgumentError)
     lambda { Car.new(:wheels => 4) }.should raise_error(ArgumentError)
   end
@@ -136,6 +136,6 @@ describe Erector::Needs do
     class ThingWithOverlap < Erector::Widget
       needs :text
     end
-    lambda { ThingWithOverlap.new(:text => "alas") }.should_not raise_error(ArgumentError)
+    lambda { ThingWithOverlap.new(:text => "alas") }.should_not raise_error
   end
 end
