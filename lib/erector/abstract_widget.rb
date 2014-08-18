cab29a219f6535645785eeccfd7b92d178ff2d6f
adamjacobbecker <ad@mbecker.cc>
2014-08-17 21:11:00 -0400

    Why would you combine two methods into one?

--- lib/erector/abstract_widget.rb
+++ lib/erector/abstract_widget.rb
@@ -71,7 +71,7 @@ module Erector
       @_parent = eval("self", block.binding) if block
       @_block = block
 
-      self.class.after_initialize(self)
+      self.class.call_after_initialize(self)
     end
 
     # Entry point for rendering a widget (and all its children). This method
--- lib/erector/after_initialize.rb
+++ lib/erector/after_initialize.rb
@@ -5,18 +5,18 @@ module Erector
     end
 
     module ClassMethods
-      def after_initialize(instance=nil, &blk)
-        if blk
-          after_initialize_parts << blk
-        elsif instance
+      def after_initialize(&blk)
+        after_initialize_parts << blk
+      end
+
+      def call_after_initialize(instance)
+        if instance
           if superclass.respond_to?(:after_initialize)
-            superclass.after_initialize instance
+            superclass.call_after_initialize instance
           end
           after_initialize_parts.each do |part|
             instance.instance_eval &part
           end
-        else
-          raise ArgumentError, "You must provide either an instance or a block"
         end
       end
 
